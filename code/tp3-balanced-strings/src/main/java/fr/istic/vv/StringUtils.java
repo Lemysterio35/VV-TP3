c51d54acbf078c55f2f898ae50be5add867c5a0b
Lemysterio35 <80769471+Lemysterio35@users.noreply.github.com>
2021-03-29 10:32:13 +0200

    Update StringUtils.java

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -5,9 +5,45 @@ public class StringUtils {
     private StringUtils() {}
 
     public static boolean isBalanced(String str) {
-        if(str.lenght % 2 != 0) { return false;
+        if(str.lenght % 2 != 0) { return null;}
+        else{
+          Stack structure = new Stack();
+            int cpt = 0;
+            while(cpt < str.lenght){
+            if(str.charAt(cpt).equals('{')){
+                structure.push('{')
+                cpt++;
+            }
+            if(str.charAt(cpt).equals('(')){
+                structure.push('(')
+                cpt++;
+            }
+            if(str.charAt(cpt).equals('[')){
+                structure.push('[')
+                cpt++;
+            }
+            String tmp = structure.peek();
+            if(str.charAt(cpt).equals('}') && tmp.equals('{')){
+                structure.pop();
+                cpt++;
+                
+             if(str.charAt(cpt).equals(')') && tmp.equals('(')){
+                structure.pop();
+                cpt++;
+                 
+             if(str.charAt(cpt).equals(']') && tmp.equals('[')){
+               structure.pop();
+                cpt++;
+             
+             else{
+                 return null;
+                
+             }
+              return true;   
+        }
                                  
-        return false;
+                                 
+        
     }
 
 }
