b2b334bacf767174bf7aa0588748f33c50c65ac4
Lemysterio35 <80769471+Lemysterio35@users.noreply.github.com>
2021-03-29 09:56:21 +0200

    Update StringUtils.java

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -5,6 +5,8 @@ public class StringUtils {
     private StringUtils() {}
 
     public static boolean isBalanced(String str) {
+        if(str.lenght % 2 != 0) { return false;
+                                 
         return false;
     }
 
